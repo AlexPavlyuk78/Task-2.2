6e7e475c809c078068969453e59d5901899a35c0
Alex78ol <119539530+Alex78ol@users.noreply.github.com>
2023-01-01 22:09:10 +0100

    Set Screen colors

--- Task 1/ViewController.swift
+++ Task 1/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let currentAlpha: CGFloat = 0.0
+    
+    @IBOutlet var redColor: UIView!
+    @IBOutlet var yellowColor: UIView!
+    @IBOutlet var blueColor: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        redColor.alpha = currentAlpha
+        yellowColor.alpha = currentAlpha
+        blueColor.alpha = currentAlpha
+        startButton.layer.cornerRadius = 8
+        
+        
     }
-
-
+    
+    @IBAction func buttonClick(_ sender: UIButton) {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        if redColor.alpha == yellowColor.alpha {
+            redColor.alpha = CGFloat(MAXFLOAT)
+            blueColor.alpha = currentAlpha
+        }
+        else if redColor.alpha > currentAlpha && yellowColor.alpha == blueColor.alpha {
+            yellowColor.alpha = CGFloat(MAXFLOAT)
+            redColor.alpha = currentAlpha
+        }
+        else if yellowColor.alpha > currentAlpha && blueColor.alpha == redColor.alpha {
+            blueColor.alpha = CGFloat(MAXFLOAT)
+            yellowColor.alpha = currentAlpha
+        }
+            
+    }
 }
-
